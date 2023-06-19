d836dd3c5e500ac6740f9b7a379a69a17ebab9f5
ceciliaytsfa <201240001072@unisnu.ac.id>
2023-06-19 12:33:16 +0700

    first

---
+++ android/app/src/main/kotlin/com/khanifzyen/twitter_clone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cecilia.twitter_clone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '642c3c9f4146b4805dae';
+  static const String projectId = '6423b5d6e54796f37178';
+  static const String endPoint = 'https://baas.pasarjepara.com/v1';
+}
